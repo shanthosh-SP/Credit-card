402618a600a501e2459dc915341155e20beeb751
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-06-24 17:13:10 +0530

    Update mlstre.py

--- mlstre.py
+++ mlstre.py
@@ -144,16 +144,7 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.3,random_stat
 
 # XGBoost Model
 def train_xgboost(predictiontime):
-	# Define the hyperparameters
-hyperparameters = {
-    'n_estimators': 100,
-    'max_depth': 3,
-    'learning_rate': 0.1,
-    'subsample': 0.8,
-    'colsample_bytree': 0.8
-}
-# Create the XGBoost model with the specified hyperparameters
-	xgb = XGBClassifier(**hyperparameters)
+	xgb = XGBClassifier()
 	xgb.fit(X_train, Y_train)
 	y_pred =xgb.predict(X_test)
 	from sklearn.metrics import  accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
